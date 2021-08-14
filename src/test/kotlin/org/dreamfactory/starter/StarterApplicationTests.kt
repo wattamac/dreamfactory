48d8ea611a9dcd75c4dce8df2123ecc0c620a30e
glenn.one <glenn.one@linecorp.com>
2021-08-14 22:27:04 +0900

    initial commit

---
+++ src/test/kotlin/org/dreamfactory/starter/StarterApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.dreamfactory.starter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StarterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
